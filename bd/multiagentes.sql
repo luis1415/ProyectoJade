5989575eaa744811d913ff36e6a015715a3d14c0
Rafael Rangel S <rarangels@unal.edu.co>
2017-05-11 05:30:09 -0500

    Se añade el codigo SQL corregido

--- bd/multiagentes.sql
+++ bd/multiagentes.sql
@@ -1,93 +1,86 @@
-CREATE DATABASE IF NOT EXISTS 'multiagentes';
-USE multiagentes;
+-- CREATE DATABASE IF NOT EXISTS multiagentes;
+-- USE multiagentes;
 
-CREATE TABLE `asignatura` (
-  `id` int(6) UNSIGNED NOT NULL,
-  `descripcion` varchar(200) NOT NULL
-);
-
-
-CREATE TABLE `estudiantes` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `apellido` varchar(50) NOT NULL,
-  `id_asignatura` int(11) NOT NULL
-);
+DROP TABLE IF EXISTS estudiante_evaluacion;
+DROP TABLE IF EXISTS respuestas_estudiante_evaluacion;
+DROP TABLE IF EXISTS respuestas_pregunta;
+DROP TABLE IF EXISTS respuesta;
+DROP TABLE IF EXISTS preguntas_evaluacion;
+DROP TABLE IF EXISTS pregunta;
+DROP TABLE IF EXISTS evaluacion;
+DROP TABLE IF EXISTS asignatura;
+DROP TABLE IF EXISTS estudiantes;
 
 
-CREATE TABLE `evaluacion` (
-  `id` int(6) UNSIGNED NOT NULL,
-  `id_asignatura` int(6) DEFAULT NULL,
-  `descripcion` varchar(200) NOT NULL,
-  `simulacro` tinyint(1) DEFAULT '0',
-  `nota` float DEFAULT NULL
-);
+CREATE TABLE estudiantes (
+	id INT AUTO_INCREMENT,
+	nombre varchar(50) NOT NULL,
+	apellido varchar(50) NOT NULL,
+	PRIMARY KEY (id)
+) ENGINE=INNODB;
 
-CREATE TABLE `preguntas` (
-  `id` int(6) UNSIGNED NOT NULL,
-  `descripcion` varchar(200) NOT NULL
-);
+CREATE TABLE asignatura (
+    id INT AUTO_INCREMENT,
+  	descripcion varchar(200) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=INNODB;
+
+CREATE TABLE evaluacion (
+	id INT AUTO_INCREMENT,
+	id_asignatura INT,
+	descripcion varchar(200) NOT NULL,
+	simulacro boolean DEFAULT false,
+	PRIMARY KEY (id),
+	FOREIGN KEY (id_asignatura) REFERENCES asignatura(id) ON DELETE CASCADE
+) ENGINE=INNODB;
+
+CREATE TABLE pregunta (
+	id INT AUTO_INCREMENT,
+	descripcion varchar(200) NOT NULL,
+	PRIMARY KEY (id)
+) ENGINE=INNODB;
+
+CREATE TABLE preguntas_evaluacion (
+	id_pregunta int NOT NULL,
+	id_evaluacion int NOT NULL,
+	PRIMARY KEY (id_pregunta, id_evaluacion),
+	FOREIGN KEY (id_pregunta) REFERENCES pregunta(id) ON DELETE CASCADE,
+	FOREIGN KEY (id_evaluacion) REFERENCES evaluacion(id) ON DELETE CASCADE
+) ENGINE=INNODB;
+
+CREATE TABLE respuesta (
+	id INT AUTO_INCREMENT,
+	descripcion varchar(200) NOT NULL,
+	PRIMARY KEY (id)
+) ENGINE=INNODB;
+
+CREATE TABLE respuestas_pregunta (
+	id_pregunta int NOT NULL,
+	id_respuesta int NOT NULL,
+	correcta boolean DEFAULT false,
+	PRIMARY KEY (id_pregunta, id_respuesta),
+	FOREIGN KEY (id_respuesta) REFERENCES respuesta(id) ON DELETE CASCADE,
+	FOREIGN KEY (id_pregunta) REFERENCES pregunta(id) ON DELETE CASCADE
+) ENGINE=INNODB;
+
+CREATE TABLE respuestas_estudiante_evaluacion (
+	id_estudiante int NOT NULL,
+	id_pregunta int NOT NULL,
+	id_respuesta int NOT NULL,
+	id_evaluacion int NOT NULL,
+	PRIMARY KEY (id_estudiante, id_pregunta, id_respuesta, id_evaluacion),
+	FOREIGN KEY (id_estudiante) REFERENCES estudiantes(id) ON DELETE CASCADE,
+	FOREIGN KEY (id_pregunta) REFERENCES pregunta(id) ON DELETE CASCADE,
+	FOREIGN KEY (id_respuesta) REFERENCES respuesta(id) ON DELETE CASCADE,
+	FOREIGN KEY (id_evaluacion) REFERENCES evaluacion(id) ON DELETE CASCADE
+) ENGINE=INNODB;
 
 
-CREATE TABLE `preguntasxrespuesta` (
-  `id_pregunta` int(6) UNSIGNED DEFAULT NULL,
-  `id_respuesta` int(6) UNSIGNED DEFAULT NULL,
-  `correcta` tinyint(1) DEFAULT '0'
-);
-
-
-CREATE TABLE `respuestas` (
-  `id` int(6) UNSIGNED NOT NULL,
-  `id_evaluacion` int(11) NOT NULL
-);
-
-
-CREATE TABLE `respuestasxevaluacion` (
-  `id_respuestas` int(11) NOT NULL,
-  `id_evaluacion` int(11) NOT NULL,
-  `total` int(11) NOT NULL,
-  `correctas` int(11) NOT NULL
-);
-
-
-ALTER TABLE `asignatura`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id` (`id`);
-
-ALTER TABLE `evaluacion`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_asignatura` (`id_asignatura`);
-
-ALTER TABLE `preguntas`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id` (`id`);
-
-ALTER TABLE `preguntasxrespuesta`
-  ADD KEY `id_pregunta` (`id_pregunta`),
-  ADD KEY `id_respuesta` (`id_respuesta`);
-
-ALTER TABLE `respuestas`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id` (`id`),
-  ADD KEY `id_evaluacion` (`id_evaluacion`);
-
-ALTER TABLE `respuestasxevaluacion`
-  ADD KEY `id_respuestas` (`id_respuestas`),
-  ADD KEY `id_evaluacion` (`id_evaluacion`);
-
-ALTER TABLE `asignatura`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `evaluacion`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `preguntas`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `respuestas`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `preguntasxrespuesta`
-  ADD CONSTRAINT `preguntasxrespuesta_ibfk_1` FOREIGN KEY (`id_respuesta`) REFERENCES `respuestas` (`id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `preguntasxrespuesta_ibfk_2` FOREIGN KEY (`id_pregunta`) REFERENCES `preguntas` (`id`) ON UPDATE CASCADE;
-
+CREATE TABLE estudiante_evaluacion (
+	id_estudiante int NOT NULL,
+	id_evaluacion int NOT NULL,
+	nota float NOT NULL,
+	PRIMARY KEY (id_estudiante, id_evaluacion),
+	FOREIGN KEY (id_estudiante) REFERENCES estudiantes(id) ON DELETE CASCADE,
+	FOREIGN KEY (id_evaluacion) REFERENCES evaluacion(id) ON DELETE CASCADE
+) ENGINE=INNODB;
